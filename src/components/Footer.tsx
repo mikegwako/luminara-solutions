296352a01e47b8230cfb7cf38ab4a73e48ad99f1
Mikeallan Gwako <99401127+mikegwako@users.noreply.github.com>
2026-02-11 13:53:07 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -57,7 +57,7 @@ const Footer = () => {
               </li>
               <li className="flex items-start gap-2">
                 <MapPin className="h-4 w-4 mt-0.5 shrink-0" />
-                <span>Nairobi & Central Kenya</span>
+                <span>Kenya & East Africa</span>
               </li>
             </ul>
           </div>
